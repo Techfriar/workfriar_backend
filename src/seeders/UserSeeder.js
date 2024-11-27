1e776af825864ff12c5ffdaf762d526347421aa5
iknoul <rhmanshamil@gmail.com>
2024-11-27 17:54:40 +0530

    add test comment

--- src/seeders/UserSeeder.js
+++ src/seeders/UserSeeder.js
@@ -14,7 +14,7 @@ const hashPassword = async (password) => {
     const salt = await bcrypt.genSalt(10);
     return await bcrypt.hash(password, salt);
 };
-
+//llll
 // Seed data for users
 const seedUsers = async () => {
     try {
