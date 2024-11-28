f9db7b159d2097361135281b7c0a902b159328d5
sabarivijayan <sabari25112012@gmail.com>
2024-11-28 13:01:11 +0530

    made a couple more changes to the multiple files upload file and fixed an issue in routes and package.json

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
  import adminRouter from './admin.js'
 import projectStatusRouter from './project-status-report.js'
 import projectRouter from './project.js'
+import userRoutes from './user.js'
 //  import employeeRouter from './employee.js'
 
 const configureRoutes = (app) => {
@@ -8,7 +9,7 @@ const configureRoutes = (app) => {
     app.get('/',(req, res)=>res.status(200).json("Routes are good ..."))
     
     app.use('/api/admin', adminRouter) // Admin API routes
-    app.use('/api/user', userRouter) // Customer API routes
+    app.use('/api/user', userRoutes) // Customer API routes
     app.use('/api/project', projectRouter)
     app.use('/api/project-status-report', projectStatusRouter)
 }
