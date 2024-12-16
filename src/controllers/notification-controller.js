21690476a51319e149b9105aaaeddf7e927604aa
snehaanil18 <snehaanil03@gmail.com>
2024-12-16 12:08:10 +0530

    user notifications completed

--- src/controllers/notification-controller.js
+++ src/controllers/notification-controller.js
@@ -2,6 +2,8 @@ import NotificationRepository from "../repositories/notification-repository.js";
 import NotificationRequest from "../requests/admin/notification-request.js";
 import { CustomValidationError } from "../exceptions/custom-validation-error.js";
 import { sendNotificationToRecipient } from "../utils/sendNotificationToRecipient.js";
+import findTimezone from "../utils/findTimeZone.js";
+import getLocalDateStringForTimezone from "../utils/getLocalDateStringForTimezone.js";
 
 class NotificationController {
     static NotificationRepo = new NotificationRepository();
@@ -88,7 +90,7 @@ class NotificationController {
             // }
             // const decoded = jwt.decode(token);
             // const user_id = decoded.UserId;
-            
+
             const user_id = '6756c072ddd097b3e4bbadd5'
             if (!user_id) {
                 throw new CustomValidationError('UserId is a required field');
@@ -117,6 +119,127 @@ class NotificationController {
 
         }
         catch (err) {
+            if (err instanceof CustomValidationError) {
+                res.status(422).json({
+                    success: false,
+                    message: 'Validation error',
+                    errors: err.errors,
+                });
+            } else {
+                return res.status(500).json({
+                    success: false,
+                    message: err.message,
+                    data: [],
+                });
+            }
+        }
+    }
+
+    /**
+     * @swagger
+     * /user/all-notifications:
+     *   post:
+     *     summary: Get all notifications for a user grouped by date
+     *     tags:
+     *       - Notification
+     *     security:
+     *       - bearerAuth: []
+     *     responses:
+     *       200:
+     *         description: Successful response with notifications grouped by date
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 success:
+     *                   type: boolean
+     *                   example: true
+     *                 message:
+     *                   type: string
+     *                   example: Notifications fetched successfully
+     *                 data:
+     *                   type: array
+     *                   items:
+     *                     type: object
+     *                     properties:
+     *                       date:
+     *                         type: string
+     *                         format: date
+     *                       notifications:
+     *                         type: array
+     *                         items:
+     *                           $ref: '#/components/schemas/Notification'
+     *       422:
+     *         description: Validation error
+     *         content:
+     *           application/json:
+     *             schema:
+     *               $ref: '#/components/schemas/ValidationError'
+     *       500:
+     *         description: Server error
+     *         content:
+     *           application/json:
+     *             schema:
+     *               $ref: '#/components/schemas/ServerError'
+     */
+
+    async getAllNotification(req, res) {
+        try {
+            // const token = req.headers.authorization?.split(' ')[1];
+            // if (!token) {
+            //     return res.status(401).json({ 
+            //         status: false,
+            //         message: 'No token provided',
+            //         data: []
+            //     });
+            // }
+            // const decoded = jwt.decode(token);
+            // const user_id = decoded.UserId;
+            const user_id = '6756c072ddd097b3e4bbadd5'
+            if (!user_id) {
+                throw new CustomValidationError('UserId is a required field');
+            }
+
+            const validateUser = await NotificationRequest.validateUser(user_id);
+            if (validateUser.error) {
+                throw new CustomValidationError(validatedDates.error);
+            }
+            const timezone = await findTimezone(req);
+            let today = getLocalDateStringForTimezone(timezone, new Date());
+            today = new Date(today);
+            const yesterday = new Date(today);
+            yesterday.setDate(today.getDate() - 1);
+            const notifications = await NotificationRepository.groupNotificationsByDate(user_id, timezone);
+
+            const result = [];
+            for (const date in notifications) {
+                if (date === today.toISOString().split('T')[0]) {
+                    result.push({ date: 'Today', notifications: notifications[date] });
+                } else if (date === yesterday.toISOString().split('T')[0]) {
+                    result.push({ date: 'Yesterday', notifications: notifications[date] });
+                } else {
+                    const formattedDate = new Date(date).toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' });
+                    result.push({ date: formattedDate, notifications: notifications[date] });
+                }
+            }
+
+            if (Object.keys(result).length > 0) {
+                res.status(200).json({
+                    success: true,
+                    message: 'Notifications fetched successfully',
+                    data: result,
+                });
+            } else {
+                res.status(200).json({
+                    success: true,
+                    message: 'No notifications found',
+                    data: [],
+                });
+            }
+
+
+        } catch (err) {
             if (err instanceof CustomValidationError) {
                 res.status(422).json({
                     success: false,
--- src/repositories/notification-repository.js
+++ src/repositories/notification-repository.js
@@ -1,4 +1,5 @@
 import Notification from "../models/notifications.js";
+import mongoose from 'mongoose';
 
 class NotificationRepository{
     static async createNotification(notificationData) {
@@ -17,6 +18,58 @@ class NotificationRepository{
             throw new Error(error);
         }
     }
+
+    static async groupNotificationsByDate(user_id, timezone) {
+        const pipeline = [
+            {
+                $match: { user_id: new mongoose.Types.ObjectId(user_id) } // Filter by user ID
+            },
+            {
+                $addFields: {
+                    createdAt: {
+                        $ifNull: ["$createdAt", new Date("1970-01-01")] // Provide a fallback date if null
+                    }
+                }
+            },
+            {
+                $addFields: {
+                    // Adjust createdAt to the provided timezone
+                    adjustedDate: {
+                        $dateToString: {
+                            format: "%Y-%m-%d",
+                            date: "$createdAt",
+                            timezone: timezone
+                        }
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: "$adjustedDate", // Group by adjusted date
+                    notifications: { $push: "$$ROOT" } // Push all notifications for that date
+                }
+            },
+            {
+                $sort: { _id: 1 } 
+            },
+            {
+                $sort: { "notifications.createdAt": -1 }
+            }
+        ];
+
+        // Run the aggregation using Mongoose
+        const groupedNotifications = await Notification.aggregate(pipeline);
+
+        // Transform the output for better usability
+        const result = {};
+        groupedNotifications.forEach(group => {
+            result[group._id] = group.notifications;
+        });
+
+        return result;
+    }
+    
+
 }
 
 export default NotificationRepository;
