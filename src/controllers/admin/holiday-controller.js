4e8b0499995a4a4ea417205f168886444f2499bc
sabarivijayan <sabari25112012@gmail.com>
2024-12-16 12:56:23 +0530

    modified holiday response

--- src/controllers/admin/holiday-controller.js
+++ src/controllers/admin/holiday-controller.js
@@ -1,7 +1,7 @@
 import HolidayRepository from "../../repositories/admin/holiday-repository.js";
 import AddHolidayRequest from "../../requests/admin/add-holiday-request.js";
 import UpdateHolidayRequest from "../../requests/admin/update-holiday-request.js";
-import HolidayResponse from '../../responses/holiday-response.js';
+import HolidayResponse from "../../responses/holiday-response.js";
 import { CustomValidationError } from "../../exceptions/custom-validation-error.js";
 
 const holidayRepo = new HolidayRepository();
@@ -55,7 +55,7 @@ export default class HolidayController {
   async addHoliday(req, res) {
     try {
       const validatedData = await new AddHolidayRequest(req).validate();
-      
+
       const holidayDetails = await holidayRepo.addHoliday(validatedData);
 
       if (holidayDetails) {
@@ -131,75 +131,19 @@ export default class HolidayController {
       const filters = req.body;
       const holidays = await holidayRepo.getAllHolidays(filters);
 
-      const formattedHolidays = await Promise.all(
-        holidays.map(async (holiday) => await HolidayResponse.format(holiday))
-      );
+      const groupedHolidays = await HolidayResponse.groupByType(holidays);
 
       return res.status(200).json({
         status: true,
         message: "Holidays retrieved successfully.",
         data: {
-          holidays: formattedHolidays,
+          holidays: groupedHolidays,
         },
       });
     } catch (error) {
       return res.status(500).json({
         status: false,
         message: "Failed to retrieve holidays.",
-        errors: error,
-      });
-    }
-  }
-
-  /**
-   * Get Holiday By Id
-   *
-   * @swagger
-   * /holiday/get/{id}:
-   *   post:
-   *     tags:
-   *       - Holiday
-   *     summary: Get holiday by id
-   *     security:
-   *       - bearerAuth: []
-   *     parameters:
-   *       - in: path
-   *         name: id
-   *         required: true
-   *         schema:
-   *           type: string
-   *         description: Holiday ID
-   *     responses:
-   *       200:
-   *         description: Success
-   *       404:
-   *         description: Not Found
-   *       500:
-   *         description: Internal Server Error
-   */
-  async getHolidayById(req, res) {
-    try {
-      const holiday = await holidayRepo.getHolidayById(req.params.id);
-
-      if (!holiday) {
-        return res.status(404).json({
-          status: false,
-          message: "Holiday not found.",
-          data: null,
-        });
-      }
-
-      const holidayData = await HolidayResponse.format(holiday);
-
-      return res.status(200).json({
-        status: true,
-        message: "Holiday retrieved successfully.",
-        data: holidayData,
-      });
-    } catch (error) {
-      return res.status(500).json({
-        status: false,
-        message: "Failed to retrieve holiday.",
         errors: error,
       });
     }
@@ -297,6 +241,104 @@ export default class HolidayController {
         status: false,
         message: "Failed to update holiday.",
         errors: error.message || error,
+      });
+    }
+  }
+
+  /**
+ * Get Next Holidays
+ *
+ * @swagger
+ * /holiday/next:
+ *   post:
+ *     tags:
+ *       - Holiday
+ *     summary: Get the next holidays based on today's date
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               location:
+ *                 type: string
+ *                 enum: ["India", "Dubai"]
+ *                 description: Filter holidays by location (optional)
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: boolean
+ *                   description: Indicates if the request was successful
+ *                 message:
+ *                   type: string
+ *                   description: Response message
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       holiday_name:
+ *                         type: string
+ *                         description: Name of the holiday
+ *                       holiday_type:
+ *                         type: string
+ *                         enum: ["National Holiday", "Public Holiday", "Restricted Holiday", "Office Shutdown"]
+ *                         description: Type of the holiday
+ *                       start_date:
+ *                         type: string
+ *                         format: date
+ *                         description: Start date of the holiday
+ *                       end_date:
+ *                         type: string
+ *                         format: date
+ *                         description: End date of the holiday
+ *                       location:
+ *                         type: string
+ *                         enum: ["India", "Dubai"]
+ *                         description: Location where the holiday is applicable
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: boolean
+ *                   description: Indicates if the request was successful
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *                 errors:
+ *                   type: string
+ *                   description: Detailed error information
+ */
+  async getNextHolidays(req, res) {
+    try {
+      const currentDate = new Date();
+      const holidays = await holidayRepo.getNextHolidays(currentDate);
+
+      const formattedHolidays = await Promise.all(
+        holidays.map((holiday) => HolidayResponse.dashboardFormat(holiday))
+      );
+      return res.status(200).json({
+        status: true,
+        message: "Next holidays retrieved successfully.",
+        data: formattedHolidays,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: false,
+        message: "Failed to retrieve next holidays.",
+        errors: error.message,
       });
     }
   }
--- src/responses/holiday-response.js
+++ src/responses/holiday-response.js
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 export default class HolidayResponse {
     /**
      * Transform the holiday resource into an object.
@@ -9,11 +11,42 @@ export default class HolidayResponse {
           id: holiday._id,
           holiday_name: holiday.holiday_name,
           holiday_type: holiday.holiday_type,
+          holiday_date: moment(holiday.start_date).format('ddd, DD MMMM, YYYY'),
           start_date: holiday.start_date,
           end_date: holiday.end_date,
           location: holiday.location,
           createdAt: holiday.createdAt,
           updatedAt: holiday.updatedAt,
+        };
+      }
+
+      static async groupByType(holidays) {
+        const groupedHolidays = holidays.reduce((acc, holiday)=>{
+          const type = holiday.holiday_type;
+          if(!acc[type]){
+            acc[type] = [];
+          }
+          acc[type].push({
+            id: holiday._id,
+            holiday_name: holiday.holiday_name,
+            holiday_date: moment(holiday.start_date).format('ddd, DD MMMM, YYYY'),
+            start_date: holiday.start_date,
+            end_date: holiday.end_date,
+            location: holiday.location,
+          });
+          return acc;
+        }, {});
+        return Object.entries(groupedHolidays).map(([type, holidays])=>({
+          holiday_type: type,
+          holidays: holidays
+        }));
+      }
+
+      static async dashboardFormat(holiday) {
+        return {
+          holiday_id: holiday._id,
+          holiday_name: holiday.holiday_name,
+          holiday_date: moment(holiday.start_date).format('ddd, DD MMMM, YYYY'),
         };
       }
       
