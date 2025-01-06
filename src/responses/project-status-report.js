dbf186f0264354cdbe460926c02d7c58db6c0773
sabarivijayan <sabari25112012@gmail.com>
2025-01-06 18:06:37 +0530

    made changes to project status report response

--- src/responses/project-status-report.js
+++ src/responses/project-status-report.js
@@ -10,7 +10,12 @@ export default class ProjectStatusReportResponse {
 
     return {
       id: report._id,
-      project_name: project.project_name,
+      project_name: project.project_name
+      ? {
+          id: report.project_name._id,
+          name: project.project_name,
+        }
+      : null,
       project_lead: report.project_lead,
       planned_start_date: this.formatDate(project.planned_start_date),
       planned_end_date: this.formatDate(project.planned_end_date),
